758fffdd69e191efc2546c20a2b48ad310f2e154
root <root@DESKTOP-MOC38TV>
2024-03-06 15:48:28 +0100

    change the enviremant work to ubuntu 20.04

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es2021: true,
+    es6: true,
     node: true,
     jest: true,
   },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
+  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  plugins: ['import', 'jest'],
+  rules: {
+    // Add your ESLint rules here
   },
-  rules: {},
 };
 
