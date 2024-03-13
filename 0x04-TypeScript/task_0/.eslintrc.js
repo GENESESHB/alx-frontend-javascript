4291b8713e5f43287854fbe57e3370d9988d54a0
root <root@DESKTOP-MOC38TV>
2024-03-14 00:57:09 +0100

    add new tasks

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  plugins: ['import', 'jest'],
+  rules: {
+    // Add your ESLint rules here
+  },
+};
